556274b7e537fd06fc570dae1a2b4af3570339e1
Shveya <shveya@iMac-Shveya.local>
2019-10-22 21:24:54 +0300

    Added slot machine test

---
+++ slots/precache-manifest.2f1e6d373364bc6221a57ece8a2c3c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3432aa00e1a49d38180e71df9bc8b188",
+    "url": "/slots/index.html"
+  },
+  {
+    "revision": "e864e5d578ba82a10c02",
+    "url": "/slots/static/css/main.62c0bb39.chunk.css"
+  },
+  {
+    "revision": "f8a515ee27e9e5595699",
+    "url": "/slots/static/js/2.5ebdcbc5.chunk.js"
+  },
+  {
+    "revision": "e864e5d578ba82a10c02",
+    "url": "/slots/static/js/main.df2d1f17.chunk.js"
+  },
+  {
+    "revision": "85583eddb5e134c095ad",
+    "url": "/slots/static/js/runtime-main.1e7b5f15.js"
+  },
+  {
+    "revision": "565a57790e190fec06a5793e260d809f",
+    "url": "/slots/static/media/Cherry.565a5779.png"
+  }
+]);
